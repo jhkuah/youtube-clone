df533daa8ad79befad37e22c3144a073cb811199
jhkuah <kjh_1997@hotmail.com>
2023-11-27 20:36:30 +0800

    added lazy-load-image

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,23 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import "./_video.scss"
-import { AiFillEye } from 'react-icons/ai'
-import request from '../../api'
+import React, { useEffect, useState } from "react";
+import "./_video.scss";
+import { AiFillEye } from "react-icons/ai";
+import request from "../../api";
 
-import moment from 'moment/moment'
-import numeral from 'numeral'
+import moment from "moment/moment";
+import numeral from "numeral";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
-const Video = ({video}) => {
+const Video = ({ video }) => {
+  const {
+    id,
+    snippet: {
+      channelId,
+      channelTitle,
+      title,
+      publishedAt,
+      thumbnails: { medium },
+    },
+  } = video;
 
-  const {id, snippet:{channelId, channelTitle, title, publishedAt, thumbnails:{medium}}}= video
+  const [views, setViews] = useState(null);
+  const [duration, setDuration] = useState(null);
+  const [channelIcon, setChannelIcon] = useState(null);
 
-  const [views, setViews] = useState(null)
-  const [duration, setDuration] = useState(null)
-  const [channelIcon, setChannelIcon] = useState(null)
-
-  const seconds = moment.duration(duration).asSeconds()
-  const formattedDuration = moment.utc(seconds * 1000).format("mm:ss")
-  const formattedViews = numeral(views).format("0.a").toUpperCase()
-  const formattedPublishedAt = moment(publishedAt).fromNow()
+  const seconds = moment.duration(duration).asSeconds();
+  const formattedDuration = moment.utc(seconds * 1000).format("mm:ss");
+  const formattedViews = numeral(views).format("0.a").toUpperCase();
+  const formattedPublishedAt = moment(publishedAt).fromNow();
   const videoId = id?.videoId || id;
 
   useEffect(() => {
@@ -36,25 +45,27 @@ const Video = ({video}) => {
     videoDetails();
   }, [videoId]);
 
-  useEffect(()=> {
+  useEffect(() => {
     const fetchChannelIcon = async () => {
-     const {data: {items}} = await request('/channels', {
-      params: {
-        part: 'snippet',
-        id: channelId,
-      }
-     })
-     setChannelIcon(items[0].snippet.thumbnails.default)
-    }
-    fetchChannelIcon()
-  }, [channelId])
-
+      const {
+        data: { items },
+      } = await request("/channels", {
+        params: {
+          part: "snippet",
+          id: channelId,
+        },
+      });
+      setChannelIcon(items[0].snippet.thumbnails.default);
+    };
+    fetchChannelIcon();
+  }, [channelId]);
 
   return (
     <div className="video">
       <div className="video__thumbnail">
-        <img src={medium.url} alt="" />
-        <span>{formattedDuration}</span>
+        {/* <img src={medium.url} alt="" /> */}
+        <LazyLoadImage src={medium.url} effect="blur" />
+        <span className="video__thumbnail__duration">{formattedDuration}</span>
       </div>
       <div className="video__title">{title}</div>
       <div className="video__details">
@@ -64,14 +75,16 @@ const Video = ({video}) => {
         <span>{formattedPublishedAt}</span>
       </div>
       <div className="video__channel">
-        <img
-          src={channelIcon?.url}
+        {/* <img
+          // src={channelIcon?.url}
+          
           alt=""
-        />
+        /> */}
+        <LazyLoadImage src={channelIcon?.url} effect="blur" />
         <p>{channelTitle}</p>
       </div>
     </div>
   );
-}
+};
 
-export default Video
+export default Video;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import {createRoot} from 'react-dom/client';
-import React from 'react';
-import App from './App';
-import {Provider} from 'react-redux'
-import store from './redux/reducers/store'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter as Router } from 'react-router-dom';
-import './_base.scss'
+import { createRoot } from "react-dom/client";
+import React from "react";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./redux/reducers/store";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import "./_base.scss";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
